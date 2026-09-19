fd9abce56fd188a2cec3c167119c38bc29bab3b9
Aboubacar Sidik Moussa Garba <aboubacarsidikmoussagarba8720@gmail.com>
2026-09-19 03:07:34 +0100

    fix: finaliser la gestion du focus du champ de publication

--- app/src/main/java/com/thehub/hb/ui/createpost/CreatePostScreen.kt
+++ app/src/main/java/com/thehub/hb/ui/createpost/CreatePostScreen.kt
@@ -39,6 +39,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
