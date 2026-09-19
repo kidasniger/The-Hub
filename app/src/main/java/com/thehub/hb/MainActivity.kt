d8bc2592a38c801cbd5ec7c1fa45bfeb14359781
Aboubacar Sidik Moussa Garba <aboubacarsidikmoussagarba8720@gmail.com>
2026-09-19 22:59:31 +0100

    Connect root theme to persisted theme mode

--- app/src/main/java/com/thehub/hb/MainActivity.kt
+++ app/src/main/java/com/thehub/hb/MainActivity.kt
@@ -7,10 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.thehub.hb.navigation.HubNavGraph
+import com.thehub.hb.ui.theme.AppThemeMode
 import com.thehub.hb.ui.theme.FrenchHubStrings
-import com.thehub.hb.ui.theme.HubSurfaceDark
+import com.thehub.hb.ui.theme.HubBackground
+import com.thehub.hb.ui.theme.HubTextPrimary
 import com.thehub.hb.ui.theme.LocalHubStrings
 import com.thehub.hb.ui.theme.TheHubTheme
 
@@ -22,11 +26,16 @@ class MainActivity : ComponentActivity() {
         val appContainer = (application as HubApplication).container
 
         setContent {
+            val currentThemeMode by appContainer.dataStoreManager.appThemeMode.collectAsState(
+                initial = AppThemeMode.DARK
+            )
+
             CompositionLocalProvider(LocalHubStrings provides FrenchHubStrings) {
-                TheHubTheme {
+                TheHubTheme(themeMode = currentThemeMode) {
                     Surface(
                         modifier = Modifier.fillMaxSize(),
-                        color = HubSurfaceDark
+                        color = HubBackground,
+                        contentColor = HubTextPrimary
                     ) {
                         HubNavGraph(appContainer = appContainer)
                     }
