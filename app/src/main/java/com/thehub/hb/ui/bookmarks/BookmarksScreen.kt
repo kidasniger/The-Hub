979305b84a11a2f4c66b42909a4d225de5c1f1fc
Aboubacar Sidik Moussa Garba <aboubacarsidikmoussagarba8720@gmail.com>
2026-09-19 22:38:01 +0100

    Unify screen surfaces with Premium Dark design

--- app/src/main/java/com/thehub/hb/ui/bookmarks/BookmarksScreen.kt
+++ app/src/main/java/com/thehub/hb/ui/bookmarks/BookmarksScreen.kt
@@ -1,6 +1,9 @@
 package com.thehub.hb.ui.bookmarks
 
 import androidx.compose.foundation.background
+import com.thehub.hb.ui.theme.HubOutline
+import com.thehub.hb.ui.theme.HubSurface
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
