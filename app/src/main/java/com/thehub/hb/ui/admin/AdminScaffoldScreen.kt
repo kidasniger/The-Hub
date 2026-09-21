2132bae4751328fef7897c8251e413f4415c7697
Aboubacar Sidik Moussa Garba <aboubacarsidikmoussagarba8720@gmail.com>
2026-09-21 23:00:46 +0100

    fix(admin): import operations settings icon

--- app/src/main/java/com/thehub/hb/ui/admin/AdminScaffoldScreen.kt
+++ app/src/main/java/com/thehub/hb/ui/admin/AdminScaffoldScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material.icons.filled.People
 import androidx.compose.material.icons.filled.Shield
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
