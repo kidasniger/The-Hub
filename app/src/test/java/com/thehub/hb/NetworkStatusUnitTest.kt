dbc86684e921160488ef4d247e15f2966e691e55
Aboubacar Sidik Moussa Garba <aboubacarsidikmoussagarba8720@gmail.com>
2026-09-23 18:39:08 +0100

    test(p7): cover network status decision with JVM-safe inputs

--- app/src/test/java/com/thehub/hb/NetworkStatusUnitTest.kt
+++ app/src/test/java/com/thehub/hb/NetworkStatusUnitTest.kt
@@ -10,15 +10,8 @@ class NetworkStatusUnitTest {
 
     @Test
     fun internet_requires_validated_network() {
-        val internetOnly = NetworkCapabilities()
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-
-        val validatedInternet = NetworkCapabilities()
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-
-        assertFalse(NetworkStatus.hasInternet(null))
-        assertFalse(NetworkStatus.hasInternet(internetOnly))
-        assertTrue(NetworkStatus.hasInternet(validatedInternet))
+        assertFalse(NetworkStatus.hasInternet(false, false))
+        assertFalse(NetworkStatus.hasInternet(true, false))
+        assertTrue(NetworkStatus.hasInternet(true, true))
     }
 }
