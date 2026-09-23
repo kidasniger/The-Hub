c69d67f63e024452e95a25b36e85834edfcc2e8a
Aboubacar Sidik Moussa Garba <aboubacarsidikmoussagarba8720@gmail.com>
2026-09-23 18:36:18 +0100

    test(p7): use NetworkCapabilities API supported by test stubs

--- app/src/test/java/com/thehub/hb/NetworkStatusUnitTest.kt
+++ app/src/test/java/com/thehub/hb/NetworkStatusUnitTest.kt
@@ -10,14 +10,12 @@ class NetworkStatusUnitTest {
 
     @Test
     fun internet_requires_validated_network() {
-        val internetOnly = NetworkCapabilities.Builder()
+        val internetOnly = NetworkCapabilities()
             .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            .build()
 
-        val validatedInternet = NetworkCapabilities.Builder()
+        val validatedInternet = NetworkCapabilities()
             .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
             .addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-            .build()
 
         assertFalse(NetworkStatus.hasInternet(null))
         assertFalse(NetworkStatus.hasInternet(internetOnly))
