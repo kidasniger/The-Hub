b84871d7b800a2aca11347133229ee80f87b9759
Aboubacar Sidik Moussa Garba <aboubacarsidikmoussagarba8720@gmail.com>
2026-09-24 23:05:17 +0100

    fix(update): retry pending APK install when returning from settings

--- app/src/main/java/com/thehub/hb/MainActivity.kt
+++ app/src/main/java/com/thehub/hb/MainActivity.kt
@@ -26,6 +26,7 @@ import com.thehub.hb.ui.theme.HubBackground
 import com.thehub.hb.ui.theme.HubTextPrimary
 import com.thehub.hb.ui.theme.LocalHubStrings
 import com.thehub.hb.ui.theme.TheHubTheme
+import com.thehub.hb.utils.ApkInstaller
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -49,6 +50,11 @@ class MainActivity : ComponentActivity() {
     private val notificationPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { }
+
+    override fun onResume() {
+        super.onResume()
+        ApkInstaller.resumePendingInstall(this)
+    }
 
     override fun onStart() {
         super.onStart()
